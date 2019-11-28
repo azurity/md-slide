385a5f046eecf82cbff0d853d170940eb095c359
azurity <784227594@qq.com>
2019-11-28 23:32:35 +0800

    bug fix

--- public/js/oauth2.js
+++ public/js/oauth2.js
@@ -5,7 +5,7 @@ window.addEventListener(
         try {
             let res = await fetch('/oauth2/name', { cache: 'no-cache' })
             let data = await res.json()
-            if (!!data) {
+            if (!!data && !!data.name) {
                 window.oauth2 = { name: data.name }
                 let oauthNode = document.createElement('div')
                 oauthNode.innerText = window.oauth2.name
