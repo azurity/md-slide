1bf77487dfe18edca47094a81b92a6200af51d81
azurity <784227594@qq.com>
2020-08-04 20:31:43 +0800

    unified implementation

--- markdown-it-plugin/asciinema-player.js
+++ markdown-it-plugin/asciinema-player.js
@@ -1,10 +1,12 @@
-module.exports = function (code, meta) {
-    if (meta['asciinema-id'] == undefined) {
-        meta['asciinema-id'] = 0
-    } else {
-        meta['asciinema-id'] += 1
-    }
-    return `<div class="asciinema">\n<asciinema-player id="asciinema-${
-        meta['asciinema-id']
-    }" src="${code.trim()}"></asciinema-player>\n</div>`
+let asciinemaId = 0
+
+let renderer = (code) => {
+    asciinemaId++
+    return `<div class="asciinema">\n<asciinema-player id="asciinema-${asciinemaId}" src="${code.trim()}"></asciinema-player>\n</div>`
 }
+
+renderer.reset = () => {
+    asciinemaId = 0
+}
+
+module.exports = renderer
